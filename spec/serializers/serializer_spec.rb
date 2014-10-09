3c6c5eb8b58019284affe6d8642088c21fb35696
Josh McArthur <joshua.mcarthur@gmail.com>
2014-10-09 23:59:19 +1300

    Update metadata include syntax for RSpec 3

--- spec/serializers/serializer_spec.rb
+++ spec/serializers/serializer_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe "Metadata" do
-  it "should be included for spec/serializers" do
-    example.metadata[:type].should == :serializer
+  it "should be included for spec/serializers" do |example|
+    expect(example.metadata[:type]).to eq :serializer
   end
 end
